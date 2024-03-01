724650de3b54de50cf9533cc99e37d4fb2ed39a0
nando <nando@hex7.com>
2024-03-01 16:41:24 +0100

    fixes

--- sensitivity/make_cmip_chart.py
+++ sensitivity/make_cmip_chart.py
@@ -6,81 +6,53 @@ import sys
 
 
 debug = False
+d_raw = []
 api = 'https://raw.githubusercontent.com/mzelinka/cmip56_forcing_feedback_ecs/master/cmip56_forcing_feedback_ecs.json'
 results = []
 threshold = 4.75
+outfile = 'out.json'
 all_file = 'all_models.png'
 hot_file = 'hot_models.png'
 
-if debug:
-  print('args: ', sys.argv)
-  print('api: ', api)
-  print('threshold: ', threshold)
-  print('debug: ', debug)
-  print('all_file: ', all_file)
-  print('hot_file: ', hot_file)
+d_raw.append({'args': sys.argv})
+d_raw.append({'api': api})
+d_raw.append({'threshold': threshold})
+d_raw.append({'debug': debug})
+d_raw.append({'all_file': all_file})
+d_raw.append({'hot_file': hot_file})
 
 if len(sys.argv) > 1:
   all_models = True
-  print('...run all models...')
+  print('...run ALL models...')
 else:
   all_models = False
-  print('...run hot models...')
+  print('...run HOT models...')
 
 r = requests.get(api)
 j = json.loads(r.text)
 
-if debug:
-  print('status', r.status_code)
-  print('text', r.text)
-  print('content', r.content)
+d_raw.append({'status': r.status_code})
 
 for key1, val1 in j.items():
-  if debug:
-    print('key1: ', key1)
-    print('val1: ', val1)
   for key2, val2 in val1.items():
-    if debug:
-      print('key2: ', key2)
-      print('val2: ', val2)
-
     if isinstance(val2, dict):
       for key3, val3 in val2.items():
         if isinstance(val3, dict):
-          if debug:
-            print('========================')
-            print('key2: ', key2)
-            print('key3: ', key3)
-            print('data: ', val3)
-            print('------------------------')
           results.append({key2: val3})
 
-if debug:
-  pprint.pprint(results)
-
-
+d_raw.append({'results': results})
 x_name = []
 for model in results:
-  if debug:
-    print('model', model)
   for name, data in model.items():
-    if debug:
-      print('name', name)
-      print('data', data)
-    for key, value in data.items():
-      if debug:
-        print('key: ', key)
-        print('value: ', value)
-      if key == 'ECS':
-        if value > threshold:
-          if debug:
-            print('name: ', name)
-          plt.bar(name[0:2], value)
+    for model_type, model_data in data.items():
+      if model_type == 'ECS':
+        if model_data > threshold:
+          plt.bar(name[0:2], model_data)
           x_name.append(name)
-        elif value > threshold - 1:
-          plt.bar(name[0:2], value)
+        elif model_data > threshold - 1:
+          plt.bar(name[0:2], model_data)
         elif all_models == True:
-          plt.bar(name[0:2], value)
+          plt.bar(name[0:2], model_data)
 
 plt.legend(set(x_name))
 plt.ylabel('ECS DATA')
@@ -92,4 +64,11 @@ if all_models:
 else:
   plt.xlabel('HOT MODELS')
   print('save', hot_file)
-  plt.savefig(hot_file)
+plt.savefig(hot_file)
+
+j = json.dumps(str(d_raw))
+print('saving ' + outfile)
+with open(outfile, 'w', encoding='utf-8') as f:
+  json.dump(j, f, ensure_ascii=False, indent=4)
+
+
