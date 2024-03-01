776e5fca45a9f87ed3771f54eebc8ee1f289ab17
nando <nando@hex7.com>
2024-03-01 14:36:15 +0100

    ecs

---
+++ sensitivity/README.md
@@ -0,0 +1,5 @@
+# ECS Equilibrium Climate Sensitivity
+
+<p>
+  <img src=https://github.com/nand0p/heat/blob/master/sensitivity/hot_models.png?raw=true
+<p>
---
+++ sensitivity/make_cmip_chart.py
@@ -0,0 +1,67 @@
+import matplotlib.pylab as plt
+import requests
+import pprint
+import json
+
+debug = False
+results = []
+threshold = 4.75
+
+r = requests.get('https://raw.githubusercontent.com/mzelinka/cmip56_forcing_feedback_ecs/master/cmip56_forcing_feedback_ecs.json')
+
+j = json.loads(r.text)
+
+if debug:
+  print('status', r.status_code)
+  print('text', r.text)
+
+for key1, val1 in j.items():
+  if debug:
+    print('key1: ', key1)
+    print('val1: ', val1)
+  for key2, val2 in val1.items():
+    if debug:
+      print('key2: ', key2)
+      print('val2: ', val2)
+
+    if isinstance(val2, dict):
+      for key3, val3 in val2.items():
+        if isinstance(val3, dict):
+          if debug:
+            print('========================')
+            print('key2: ', key2)
+            print('key3: ', key3)
+            print('data: ', val3)
+            print('------------------------')
+          results.append({key2: val3})
+
+if debug:
+  pprint.pprint(results)
+
+
+x_name = []
+for model in results:
+  if debug:
+    print('model', model)
+  for name, data in model.items():
+    if debug:
+      print('name', name)
+      print('data', data)
+    for key, value in data.items():
+      if debug:
+        print('key: ', key)
+        print('value: ', value)
+      if key == 'ECS':
+        if value > threshold:
+          if debug:
+            print('name: ', name)
+          plt.bar(name[0:2], value)
+          x_name.append(name)
+        elif value > threshold - 1:
+          plt.bar(name[0:2], value)
+
+plt.legend(x_name)
+plt.xlabel('HOT MODELS')
+plt.ylabel('ECS DATA')
+plt.title('EQUILIBRIUM CLIMATE SENSITIVITY')
+plt.savefig('hot_models.png')
